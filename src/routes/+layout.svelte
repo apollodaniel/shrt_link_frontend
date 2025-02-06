8d150175f5fb9cca2c2f6cee9c993cb946c36d0f
Apollo Daniel <developer.apollo.mail@gmail.com>
2025-02-06 12:32:15 -0300

    Fixed layout on mobile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,32 @@
 <script lang="ts">
-	import { Button } from '@sveltestrap/sveltestrap';
+	import { Button, Icon } from '@sveltestrap/sveltestrap';
 	import { onMount } from 'svelte';
 
 	const { data } = $props();
+	let isNavVisible = $state(false);
 </script>
 
 {#if !data.isExcludedLayout}
-	<nav>
-		<h1 href="/">
-			shrt <span>link</span>
-		</h1>
-		<ul>
-			<li><a href="/">Home</a></li>
-			<li><a href="/pricing">Pricing</a></li>
-			<li><a href="/features">Features</a></li>
-			<li><a href="/about">About</a></li>
+	<nav class={isNavVisible ? 'visible' : ''}>
+		<div class="logo">
+			<h1 href="/">
+				shrt <span>link</span>
+			</h1>
+
+			<button
+				class="btn btn-primary opendrawer"
+				onclick={() => (isNavVisible = !isNavVisible)}
+				><Icon
+					class="d-flex justify-content-center align-items-center fs-5 fw-normal"
+					name="list"
+				/></button
+			>
+		</div>
+		<ul class={isNavVisible ? 'visible' : ''}>
+			<li><a onclick={() => (isNavVisible = false)} href="/">Home</a></li>
+			<li><a onclick={() => (isNavVisible = false)} href="/pricing">Pricing</a></li>
+			<li><a onclick={() => (isNavVisible = false)} href="/features">Features</a></li>
+			<li><a onclick={() => (isNavVisible = false)} href="/about">About</a></li>
 			<Button
 				class="ms-3"
 				color="primary"
@@ -67,14 +79,73 @@
 		text-decoration: none;
 		color: #0d6efd;
 	}
-	nav > h1 {
+	h1 {
 		font-weight: bold;
 	}
 
-	nav > h1 > span {
+	h1 > span {
 		font-weight: normal;
 	}
-	nav > h1 {
+	h1 {
 		cursor: pointer;
+	}
+
+	.opendrawer {
+		display: none;
+	}
+
+	@media (max-width: 650px) {
+		.logo {
+			width: 100%;
+			height: 80px;
+			justify-content: center;
+			align-items: center;
+			display: flex;
+			z-index: 10;
+			background-color: white;
+			padding-top: 24px;
+		}
+		nav {
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+			padding: 0px;
+		}
+	}
+	@media (max-width: 510px) {
+		.logo {
+			position: absolute;
+			top: 0px;
+			justify-content: space-between;
+			padding-inline: 32px;
+		}
+		.opendrawer {
+			display: block;
+			width: 48px;
+			height: 48px;
+		}
+		ul {
+			height: 150px;
+			width: 250px;
+			transition: top ease-in-out 500ms;
+			top: 90px;
+			position: absolute;
+			text-align: end;
+		}
+		nav {
+			height: 100px;
+			transition: height ease-in-out 500ms;
+		}
+		nav.visible {
+			height: 450px;
+			justify-content: start;
+			transition: height ease-in-out 500ms;
+		}
+		ul:not(.visible) {
+			top: -150px;
+		}
+		nav > ul {
+			flex-direction: column;
+		}
 	}
 </style>
